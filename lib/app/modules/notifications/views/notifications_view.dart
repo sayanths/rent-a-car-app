6a16a6f4dda453116fa9fc0284ebbd0266ce56eb
sayanth A <sayanthsayu619@gmail.com>
2022-09-03 18:40:27 +0530

    splash screen slightly completed

--- lib/app/modules/notifications/views/notifications_view.dart
+++ lib/app/modules/notifications/views/notifications_view.dart
@@ -22,15 +22,16 @@ class NotificationsView extends GetView<NotificationsController> {
                   maxHeight: size.height,
                   child: ListView.separated(
                     separatorBuilder: (context, index) {
-                      return Divider(height: 5,color: white,);
+                      return Divider(
+                        height: 5,
+                        color: white,
+                      );
                     },
                     itemCount: 5,
                     itemBuilder: (BuildContext context, int index) {
                       return ListTile(
-                        leading: Icon(
-                          Icons.sim_card_alert,
-                          color: white,
-                        ),
+                        leading: Image.network(
+                            "https://res.cloudinary.com/sayanth/image/upload/v1662206280/zara%27s%20shopping%20app/zara%20shopping/notifcation_udbmy3.png"),
                         title: Text(
                           "Onam Special Offer",
                           style: gfontsubtitlefont(
